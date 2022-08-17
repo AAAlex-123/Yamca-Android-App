64bb4ff670a43265a8904dcaee0de352ffbc6c0e
Alex Mandelias <alexmandelias@gmail.com>
2022-08-17 19:14:48 +0300

    integrates LG class with Logcat

--- app/src/main/java/alexman/yamca/app/UserSingleton.kt
+++ app/src/main/java/alexman/yamca/app/UserSingleton.kt
@@ -4,9 +4,38 @@ import alexman.yamca.eventdeliverysystem.client.IUser
 import alexman.yamca.eventdeliverysystem.client.IUserHolder
 import alexman.yamca.eventdeliverysystem.client.User
 import alexman.yamca.eventdeliverysystem.dao.IProfileDAO
+import alexman.yamca.eventdeliverysystem.util.LG
+import android.util.Log
+import java.io.OutputStream
+import java.io.PrintStream
 import java.net.InetAddress
 
 object UserSingleton : IUser by Holder.get() {
+
+    init {
+        val tag = "LG"
+
+        val outputStream = object : PrintStream(object : OutputStream() {
+            override fun write(b: Int) {}
+        }) {
+            override fun printf(format: String, vararg args: Any): PrintStream {
+                Log.d(tag, String.format(format, *args))
+                return this
+            }
+        }
+
+        val errorStream = object : PrintStream(object : OutputStream() {
+            override fun write(b: Int) {}
+        }) {
+            override fun printf(format: String, vararg args: Any): PrintStream {
+                Log.e(tag, String.format(format, *args))
+                return this
+            }
+        }
+
+        LG.setOut(outputStream)
+        LG.setErr(errorStream)
+    }
 
     object Holder : IUserHolder {
 
